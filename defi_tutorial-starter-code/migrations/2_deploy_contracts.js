9dbe0ecbff58abaf877ce7a3649d775b0e8becc7
NathanFrank285 <nathansfrank@yahoo.com>
2021-08-09 17:26:26 -0700

    setting constructor and initial transfer of tokens into the token farm

--- defi_tutorial-starter-code/migrations/2_deploy_contracts.js
+++ defi_tutorial-starter-code/migrations/2_deploy_contracts.js
@@ -1,5 +1,25 @@
-const TokenFarm = artifacts.require("TokenFarm")
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm)
-}
+module.exports = async function(deployer, network, accounts) {
+
+  // deploy mock DAI token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed()
+
+  // deploy DappToken
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+
+
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed()
+
+  // tranfer all tokens to TokenFarm (1mm)
+  await dappToken.transfer(tokenFarm.address, "1000000000000000000000000");
+
+  // Transfer 100 Mock Dai to investor
+  await daiToken.transfer(accounts[1].address, "100000000000000000000");
+};
