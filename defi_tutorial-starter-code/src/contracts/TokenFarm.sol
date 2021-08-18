7b283c3e6ca6e6a766ff54b6d64caa4bb20fb1da
NathanFrank285 <nathansfrank@yahoo.com>
2021-08-17 17:55:18 -0700

    adding the staking balance map and stakeTokens function

--- defi_tutorial-starter-code/src/contracts/TokenFarm.sol
+++ defi_tutorial-starter-code/src/contracts/TokenFarm.sol
@@ -7,12 +7,27 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    mapping (address => uint) public stakingBalance;
 
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
 
+    // 1. Stake tokens, put money into the app, deposit
+    function stakeTokens(uint _amount) public {
+        // transfer mock dai to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        
+    }
+
+    // 2. Unstake tokens, take money from the app, withdraw
+
+
+    // 3. Issuing tokens, earn interest from staking
 
 
 }
