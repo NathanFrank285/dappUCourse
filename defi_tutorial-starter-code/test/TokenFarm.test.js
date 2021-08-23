112372c61700fcd8a634442edd6b017a12a364ba
NathanFrank285 <nathansfrank@yahoo.com>
2021-08-23 16:57:29 -0700

    added tests for ustaking

--- defi_tutorial-starter-code/test/TokenFarm.test.js
+++ defi_tutorial-starter-code/test/TokenFarm.test.js
@@ -95,10 +95,20 @@ contract('TokenFarm', ([owner, investor])=>{
       assert.equal(result.toString(), tokens('100'),'investor Dapp Token wallet balance correct after issuance')
 
       // Ensure that only owner can issue DApp tokens
-      await tokenFarm.issueTokens({from: investor}).should.be.accepted;
+      await tokenFarm.issueTokens({from: investor}).should.be.rejected;
 
+      //unstake tokens
+      await tokenFarm.unstakeTokens({from: investor})
 
+      // check tokenFarm balance for the investor
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor Mock Dai wallet balance correct after staking')
 
+      result = await daiToken.balanceOf(tokenFarm.address)
+      assert.equal(result.toString(), tokens('0'), 'Token Farm Mock Dai balance correct after staking')
+
+      result = await tokenFarm.stakingBalance(investor)
+      assert.equal(result.toString(), tokens('0'), 'investor staking balance correct after staking')
 
     })
 
